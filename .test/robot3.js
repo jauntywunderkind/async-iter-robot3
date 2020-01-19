e110ba238d3435d42dffd2d3d5a89cb5451f6fed
jauntywunderkind <jaunty+wunder+kind+scm@voodoowarez.com>
2020-01-19 13:54:12 -0500

    proper tests in first test

--- .test/robot3.js
+++ .test/robot3.js
@@ -5,11 +5,15 @@ import tape from "tape"
 import Robot3 from "../robot3.js"
 import trafficLight from "./_fixture_traffic.js"
 
-tape( "robot3", async function( t){
+/**
+* This test walks through traffic light
+*/
+tape( "synchronous walk of traffic light", async function( t){
 	const light= new Robot3( trafficLight)
-	t.equal( light.current, "green")
+	t.equal( light.current, "green", "green")
 	light.send( "step")
+	t.equal( light.current, "yellow", "yellow")
 	await Immediate()
-	t.notEqual( light.current, "green")
+	t.equal( light.current, "red", "red")
 	t.end()
 })
